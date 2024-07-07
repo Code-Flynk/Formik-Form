a3f7ffc2ed89ca4cd717964a8cc64ba815d3f158
Code-Flynk <150760061+Code-Flynk@users.noreply.github.com>
2024-07-07 14:30:35 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,70 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import { useFormik } from "formik";
+
+function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      console.log('form:', values);
+      alert("Login Successful");
+    },
+    validate: (values) => {
+      let errors = {};
+      if (!values.email) errors.email = "Field required";
+      if (!values.password) errors.password = "Field required";
+      return errors;
+    },
+  });
+
+  return (
+
+    
+    <div>
+              <img src={logo} className="App-logo" alt="logo" />
+      <form onSubmit={formik.handleSubmit}>
+
+
+        <div>Email:</div>
+        <input
+          id="emailField"
+          type="text"
+          name="email"
+          onChange={formik.handleChange} value={formik.values.email} />
+        {formik.errors.email ? (
+          <div id="emailError" style={{ color: "red" }}>
+            {formik.errors.email}
+          </div>
+        ) : null}
+        <div>Password:</div>
+        <input
+          id="pswField"
+          type="text"
+          name="password"
+          onChange={formik.handleChange} value={formik.values.password} />
+        <br />
+        {formik.errors.password ? (
+          <div id="passWordError" style={{ color: "red" }}>
+            {formik.errors.password}
+          </div>
+        ) : null}
+
+<div>Name (Optional)</div>
+      <input id="name" type="text"/>
+
+
+
+        <button id="submitButton" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
